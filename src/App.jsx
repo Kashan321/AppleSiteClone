1d4a6abed1442647bae507f6230e379e1ed2970d
Muhammad Kashan Haider <muhammadkashanhaider3241@gmail.com>
2024-08-05 16:39:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+import Highlights from './Components/Highlights'
+import Model from './Components/Model'
+import Features from './Components/Features'
+import HowItWork from './Components/HowItWork'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWork />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
